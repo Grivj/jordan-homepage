874d138f8807ccc128f876486e50cee5ba50ea63
Jordan Griveau <jgriveau@yahoo.fr>
2022-02-10 14:51:52 +0100

    Update socials.tsx
    Update links

--- components/socials.tsx
+++ components/socials.tsx
@@ -6,10 +6,10 @@ import LinkedInIcon from "../public/socials_logos/linkedin.svg"
 
 export const SocialsBar = () => (
     <HStack justifyContent="center" pb="20px" gap="10px">
-        <Link>
+        <Link href="https://www.linkedin.com/in/griveau-jordan/" isExternal>
             <Image src={LinkedInIcon} alt="Python icon" width="32px" height="32px" />
         </Link>
-        <Link>
+        <Link href="https://github.com/Grivj" isExternal>
             <Image src={GitHubIcon} alt="Python icon" width="32px" height="32px" />
         </Link>
     </HStack>
